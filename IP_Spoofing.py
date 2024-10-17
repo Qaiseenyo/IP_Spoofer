432f12874fe0b2911f8c04a5f6bc2bf663907eb6
Qaiseenyo <qaishammad2004@gmail.com>
2024-10-17 19:51:45 +0300

    Add files via upload

---
+++ IP_Spoofing.py
@@ -0,0 +1,49 @@
+import sys
+from scapy.all import sr, IP, ICMP
+from faker import Faker
+from colorama import Fore, init
+
+#Initialize colorama for colored console output
+
+init()
+#Create a faker object for generating fake data
+
+fake = Faker()
+
+#Function to generate a fake IPv4 address
+
+def generate_fake_ip():
+    return fake.IPv4()
+
+def craft_and_send_packet(source_ip, destination_ip):
+    
+    #Craft an ICMP packet with the specified source and destination IP
+
+    packet  = IP(src=source_ip, dst=destination_ip) / ICMP()
+    
+    #Send and receive the packet with a timeout.
+
+    answers, _ = sr(packet, verbose=0, timeout=5)
+    return answers
+
+    #Function to display a summary of the sent and recieved packets.
+
+def display_packet_summary(sent, recieved):
+    print(f"{Fore.GREEN}[+]")
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+   print(f"{Fore.RED}[-] Error! {Fore.GREEN} Please run as: {sys.argv[0]} <dst_ip>")
+   sys.exit(1)
+
+# Retrieve the destination IP from the command-line arguments
+destination_ip = sys.argv[1]
+# Generate a fake source IP
+source_ip = generate_fake_ip()
+
+# Craft and send the packet, and receive the response
+answers = craft_and_send_packet(source_ip, destination_ip)
+
+# Display the packet summary for each sent and received pair
+for sent, received in answers:
+    display_packet_summary(sent, received)
